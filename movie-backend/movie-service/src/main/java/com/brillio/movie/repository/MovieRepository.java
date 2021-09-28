2312419050ceb9af23c0e64eab97a9834c8cb6ae
unknown <4ssiddharth@gmail.com>
2021-09-28 19:05:02 +0530

    added movie service

---
+++ movie-backend/movie-service/src/main/java/com/brillio/movie/repository/MovieRepository.java
@@ -0,0 +1,19 @@
+package com.brillio.movie.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brillio.movie.model.Movie;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, String> {
+
+	Optional<Movie> findByMovieId(String movieName);
+
+//	Movie findByMovieName(String movieName);
+
+
+}
