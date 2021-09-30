27659e6066b44a15db7e07fdc187d3601f345fd6
AmalKrishna.A <AmalKrishna.A@brillio.com>
2021-09-30 13:23:25 +0530

    Added @CrossOrigin

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "./css/main.css";
 
 import AuthService from "./services/auth.service";
 
 import Login from "./components/login.component";
 import Register from "./components/register.component";
-import Home from "./components/home.component";
+import Home from "./components/Home";
 import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 
