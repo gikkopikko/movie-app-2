34c7bad2825ee2ebe9dffb3c77d70a75e8bcf1f1
Alankrita2 <49248626+Alankrita2@users.noreply.github.com>
2021-10-01 13:47:36 +0530

    Create MovieDescription.jsx

---
+++ movie-frontend/src/components/MovieDescription.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import  "../css/moviedescription.css";
+
+export default class MovieDesc extends Component {
+  state = {
+    movie: {}
+  }
+
+  componentDidMount() {
+
+    console.log(this.props);
+
+    axios.get(`http://localhost:9095/movies/`+this.props.match.params.id)
+      .then(res => {
+        this.setState({ movie : res.data});
+      })
+  }
+  render() {
+    return (
+      <body>
+        <div>
+            <header classname="header">
+                <nav className="navbar">
+
+                    <a className="Links" >Home</a>
+                    <a className="Links" >Profile</a>
+                </nav>
+            </header>
+            
+            <div className="container">
+                <div className="col1">
+                    <img src={this.state.movie.posterURL} className="img-demo"/>
+                </div>
+                <div className="col2">
+                    <h1>MOVIE DESCRIPTION</h1>
+                    <br/>
+                    <p>{this.state.movie.description}</p>
+                    <div className="col2-col1">
+                        <h3>Movie Name:<span>{this.state.movie.movieName}</span></h3>
+                        <h3>Category:<span>{this.state.movie.category}</span></h3>
+                        <h3>Actors:<span>{this.state.movie.actors}</span></h3>
+                        <h3>Rating:<span>{this.state.movie.rating}</span></h3>
+                    </div>
+                    <div className="col2-col2">
+                        <h3>Director:<span>{this.state.movie.director}</span></h3>
+                        <h3>Price:<span>{this.state.movie.price}</span></h3>
+                        <h3>Seats Remaining:<span>{60-this.state.movie.totalSeatsOccupied}</span></h3>
+                    </div>
+                    <div className="col2-col3">
+                    <button>Book your ticket now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </body>
+    );
+  }
+}
