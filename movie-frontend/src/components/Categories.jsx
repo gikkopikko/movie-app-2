cb2ebfff6523040518eb2de29cf52093c4d26b3e
AmalKrishna.A <AmalKrishna.A@brillio.com>
2021-10-03 17:22:40 +0530

    Fetch categories from DB

--- movie-frontend/src/components/Categories.jsx
+++ movie-frontend/src/components/Categories.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Category from "./Category";
 
 export default function Categories() {
+
+    const [categories,setCategories] = useState([]);
+
+    useState(()=>{
+        fetch("http://localhost:9095/allCategoryDetails")
+        .then(res => res.json())
+        .then(data => {
+            let cat = [];
+            data.forEach((obj) => {
+                cat.push(obj.category);
+            });
+            setCategories(cat);
+        });
+    },[]);
+
     return (
         <div>
-            <Category title="Action"/>
-            <Category title="Horror"/>
-            <Category title="Sci-fi"/>
+            {categories.map((cat) => {
+                return <Category title={cat.toUpperCase()}/>
+            })}
         </div>
     )
 }
--- movie-frontend/src/components/login.component.js
+++ movie-frontend/src/components/login.component.js
@@ -7,6 +7,9 @@ import CheckButton from "react-validation/build/button";
 // import { input } from "react-validation/build/input";
 // import { form } from "react-validation/build/form";
 import { login } from "../common/api-utils";
+
+import "../css/login.css";
+
 const ACCESS_TOKEN = "accessToken";
 const required = (value) => {
   if (!value) {
